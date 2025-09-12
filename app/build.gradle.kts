5c9c78bd78cb3beb45ec6f25de634e2f4c45a829
Jiae <jiae4042@naver.com>
2025-09-13 03:22:38 +0900

    버전 오류 해결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")      
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.example.antibrokedswu"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.antibrokedswu"
@@ -45,4 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
 }
