f325185b259392679c846bafea289f55dab235f7
Jeongchaelee <148073079+Jeongchaelee@users.noreply.github.com>
2025-09-19 22:56:56 +0900

    dkdk

---
+++ app/src/main/java/com/example/antibrokedswu/mbti/MyConsumeMbti.kt
@@ -0,0 +1,11 @@
+package com.example.antibrokedswu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyConsumeMbtiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_myconsumembti)
+    }
+}
