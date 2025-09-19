805b2583306e4fca2021ae900eea8be5ad4e2702
lunes22 <145524022+lunes22@users.noreply.github.com>
2025-09-19 23:58:34 +0900

    임시 커밋2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
         id("com.android.application") version "8.9.1"
         id("com.android.library")     version "8.9.1"
         id("org.jetbrains.kotlin.android") version "1.9.24"
+        //id("org.jetbrains.kotlin.android") version "2.0.21" //언제 작성 된지 모르는 라인... 증말 별별 에러로 괴롭힘;;
     }
 }
 dependencyResolutionManagement {
