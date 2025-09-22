b444eb4db63f8d70277954323c740a5547789f55
Jiae <jiae4042@naver.com>
2025-09-22 20:14:00 +0900

    survey activity -> mbti fragment

--- app/src/main/java/com/example/antibrokedswu/mbti/MbtiFragment.kt
+++ app/src/main/java/com/example/antibrokedswu/mbti/MbtiFragment.kt
@@ -1,6 +1,21 @@
 package com.example.antibrokedswu
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class MbtiFragment : Fragment(R.layout.fragment_mbti) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val nextButton: Button = view.findViewById(R.id.btn_next)
+
+        nextButton.setOnClickListener {
+            val intent = Intent(requireContext(), MyConsumeMbtiActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
