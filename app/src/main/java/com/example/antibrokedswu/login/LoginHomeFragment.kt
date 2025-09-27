6a6ff62346c19e94847f60d3d89561e94a407e5b
lunes22 <145524022+lunes22@users.noreply.github.com>
2025-09-27 18:36:18 +0900

    CustomToast 생성 및 적용 로그인/회원가입 기능 이메일 제외 페이지이동 비활성화

--- app/src/main/java/com/example/antibrokedswu/login/LoginHomeFragment.kt
+++ app/src/main/java/com/example/antibrokedswu/login/LoginHomeFragment.kt
@@ -1,12 +1,17 @@
 package com.example.antibrokedswu
 
+import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.antibrokedswu.databinding.FragmentLoginHomeBinding
 
 
-class LoginHomeFragment :  Fragment(R.layout.fragment_login_home){
+class LoginHomeFragment : Fragment(R.layout.fragment_login_home) {
     private var _binding: FragmentLoginHomeBinding? = null
     private val binding get() = _binding!!
 
@@ -22,24 +27,45 @@ class LoginHomeFragment :  Fragment(R.layout.fragment_login_home){
         }
 
         binding.btnGoogle.setOnClickListener { //현재는 이메일 로그인으로 연결, 추후 기능 수정
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.container, LoginByEmailFragment())
-                .addToBackStack(null)
-                .commit()
+            showCustomToast(requireContext(), "현재 준비 중인 서비스 입니다 :)!")
+//            parentFragmentManager.beginTransaction()
+//                .replace(R.id.container, LoginByEmailFragment())
+//                .addToBackStack(null)
+//                .commit()
         }
 
-        binding.btnKakao.setOnClickListener { //현재는 이메일 로그인으로 연결, 추후 기능 수정
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.container, LoginByEmailFragment())
-                .addToBackStack(null)
-                .commit()
+        binding.btnKakao.setOnClickListener {
+            showCustomToast(requireContext(), "현재 준비 중인 서비스 입니다 :)!")
+////            parentFragmentManager.beginTransaction()
+////                .replace(R.id.container, LoginByEmailFragment())
+////                .addToBackStack(null)
+////                .commit()
         }
 
         binding.btnNaver.setOnClickListener { //현재는 이메일 로그인으로 연결, 추후 기능 수정
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.container, LoginByEmailFragment())
-                .addToBackStack(null)
-                .commit()
+            showCustomToast(requireContext(), "현재 준비 중인 서비스 입니다 :)!")
+//            parentFragmentManager.beginTransaction()
+//                .replace(R.id.container, LoginByEmailFragment())
+//                .addToBackStack(null)
+//                .commit()
         }
+    }
+
+    fun showCustomToast(context: Context, message: String) {
+        val inflater = LayoutInflater.from(context)
+        val layout: View = inflater.inflate(R.layout.custom_toast, null)
+
+        val textView = layout.findViewById<TextView>(R.id.toast_text)
+        textView.text = message
+
+        val toast = Toast(context)
+
+        toast.duration = Toast.LENGTH_SHORT
+
+        toast.setGravity(Gravity.BOTTOM or Gravity.CENTER_HORIZONTAL, 0, 100)
+
+        toast.view = layout
+
+        toast.show()
     }
 }
